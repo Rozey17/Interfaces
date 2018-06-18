2264b4d7fce58b05717ece4fc871c01ee56094e3
ferrad.fm@gmail.com <ferrad.fm@gmail.com>
2018-06-18 11:56:30 +0200

    Project finished

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,6 +10,17 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            WorkflowEngine workflowEngine = new WorkflowEngine();            
+
+            UpLoadVideo activity = new UpLoadVideo();
+
+            SayHi sayHi = new SayHi();
+
+            workflowEngine.Run(sayHi);
+
+            //workflowEngine.Run(activity);
+
+            Console.ReadLine();
         }
     }
 }
